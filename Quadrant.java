d6fd9086fa6cd1f3f98a4b712711b677f8e652c5
chrleshua <charleshuang100@gmail.com>
2022-12-31 17:42:17 -0800

    Create Quadrant.java

---
+++ Quadrant.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Quadrant {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String xInput = sc.nextLine();
+        String yInput = sc.nextLine();
+        double x = Double.parseDouble(xInput);
+        double y = Double.parseDouble(yInput);
+        if (x > 0) {
+            if (y > 0) {
+                System.out.println("Quadrant 1");
+            } else if {
+                if (y < 0 ) {
+                    System.out.println("Quadrant 4");
+                }
+            }
+        } else if (x < 0) {
+            if (y > 0) {
+                System.out.println("Quadrant 2");
+            } else if (y < 0) {
+                System.out.println("Quadrant 3");
+            }
+        }
+        else if (x==0 || y==0) {
+            System.out.println("Not in a quadrant.");
+        }
+
+    }
+
+}
